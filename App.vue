4bae18c998490f1ef9fcad51b6199501f6bf54dc
Dennis Karungu <33949677+dennis2018@users.noreply.github.com>
2020-03-23 09:16:56 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+    <view>
+        <header/>
+        <scroll-view class="scroll-view">
+            <gif-item v-for="gif in gifs" :gif="gif" :key="gif.id" v-if="!loading"/>
+            <view class="loading-container" :style="{flex: 1, justifyContent: 'center'}" v-if="loading">
+                <activity-indicator size="large" color="#0000ff"></activity-indicator>
+            </view>
+        </scroll-view>
+    </view>
+</template>
+
+<script>
+  import Giphy from 'giphy-js-sdk-core';
+  import GifItem from './components/GifItem';
+  import Header from './components/header';
+
+  const client = Giphy('TxuQbNU1nyDBwpqrcib61LxmOzsXTPEk');
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        gifs: [],
+        loading: true
+      };
+    },
+    async created() {
+      const response = await client.trending('gifs', {limit: 20});
+      this.gifs = response.data;
+      this.loading = false;
+    },
+    components: {GifItem, Header}
+  };
+</script>
+
+<style>
+    .scroll-view {
+        padding-top: 20px;
+        padding-bottom: 30px;
+    }
+
+    .loading-container {
+        height: 600px;
+    }
+</style>
